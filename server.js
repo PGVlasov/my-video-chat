e886eb2d51a544282fd5e51837a05754f1091c38
PGVlasov <PGVlasov@users.noreply.github.com>
2022-05-04 13:06:18 +0300

    first version is ready

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function getClientRooms() {
   const { rooms } = io.sockets.adapter;
 
   return Array.from(rooms.keys()).filter(
-    (roomID) => validate() && version === 4
+    (roomID) => validate(roomID) && version(roomID) === 4
   );
 }
 
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
     if (Array.from(joinedRooms).includes(roomID)) {
       return console.warn(`Already connected to room ${roomID}`);
     }
-    const clients = Array.from(io.sockets.adapter.rooms.get(roomID || []));
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
 
     clients.forEach((clientID) => {
       io.to(clientID).emit(ACTIONS.ADD_PEER, {
@@ -51,27 +51,52 @@ io.on("connection", (socket) => {
   function leaveRoom() {
     const { rooms } = socket;
 
-    Array.from(rooms).forEach((roomID) => {
-      const clients = Array.from(io.sockets.adapter.rooms.get(roomID || []));
+    Array.from(rooms)
+      // LEAVE ONLY CLIENT CREATED ROOM
+      .filter((roomID) => validate(roomID) && version(roomID) === 4)
+      .forEach((roomID) => {
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
 
-      clients.forEach((clientID) => {
-        io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
-          peerID: socket.id,
+        clients.forEach((clientID) => {
+          io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+            peerID: socket.id,
+          });
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          });
         });
 
-        socket.emit(ACTIONS.REMOVE_PEER, {
-          peerID: clientID,
-        });
+        socket.leave(roomID);
       });
-
-      socket.leave(roomID);
-    });
 
     shareRoomsList();
   }
 
   socket.on(ACTIONS.LEAVE, leaveRoom);
   socket.on("disconnecting", leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({ peerID, sessionDescription }) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
+    });
+  });
+
+  socket.on(ACTIONS.RELAY_ICE, ({ peerID, iceCandidate }) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+});
+
+const publicPath = path.join(__dirname, "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 server.listen(PORT, () => console.log(`Server is started on PORT ${PORT}`));
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import ACTIONS from "./socket/actions";
 import { socket } from "./socket/socket";
@@ -8,12 +8,15 @@ import { useNavigate } from "react-router";
 export const Main = () => {
   const [rooms, setRooms] = useState([]);
   const history = useNavigate();
+  const rootNode = useRef();
 
   useEffect(() => {
     socket.on(ACTIONS.SHARE_ROOMS, ({ rooms = [] } = {}) => {
-      setRooms(rooms);
+      if (rootNode) {
+        setRooms(rooms);
+      }
     });
-  }, [rooms]); /// chek it
+  }, [rooms]);
 
   const joinRoomHandler = (roomID) => {
     console.log("Join");
@@ -26,7 +29,7 @@ export const Main = () => {
   };
 
   return (
-    <div style={{ margin: "2rem" }}>
+    <div style={{ margin: "2rem", alignItems: "center" }}>
       <h2>Avaible Rooms</h2>
       <ul
         style={{
@@ -35,23 +38,10 @@ export const Main = () => {
         }}
       >
         {rooms.map((roomID) => (
-          <Card
-            style={{
-              width: "90%",
-              textAlign: "center",
-              margin: "1rem",
-            }}
-            key={roomID}
-          >
-            <Card.Body
-              style={{
-                display: "column",
-                width: "90%",
-                textAlign: "center",
-              }}
-            >
-              {roomID}
-              <p>test</p>
+          <Card key={roomID} style={{ marginTop: "1rem" }}>
+            <Card.Body>
+              <p>{roomID}</p>
+
               <Button
                 style={{ width: "18rem", marginTop: "2rem" }}
                 onClick={() => joinRoomHandler(roomID)}
