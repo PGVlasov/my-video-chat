3fc6b30632fab9803f1850801d6649eab62c125f
PGVlasov <PGVlasov@users.noreply.github.com>
2022-05-05 12:57:12 +0300

    port is changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(server);
 const ACTIONS = require("./src/components/socket/actions");
 const { version, validate } = require("uuid");
 
-const PORT = process.env.PORT || 3011;
+const PORT = process.env.PORT || 3050;
 
 function getClientRooms() {
   const { rooms } = io.sockets.adapter;
--- src/components/socket/socket.js
+++ src/components/socket/socket.js
@@ -7,4 +7,4 @@ const options = {
   transports: ["websocket"],
 };
 
-export const socket = io("ws://193.168.48.104:3011/", options); //"http://localhost:3011"
+export const socket = io("ws://193.168.48.104:3050/", options); //"http://localhost:3011" "ws://193.168.48.104:3011/"
